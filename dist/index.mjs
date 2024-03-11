f0854902f60d005371ad7b0bb8892d4419c4a6d4
Cain <75994858+cainthebest@users.noreply.github.com>
2024-03-11 08:16:19 +0000

    fix: user side imports for module

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,2 +1,9 @@
 import punify from "./index.js";
+
+export const toAscii = punify.toAscii;
+export const toAsciiStrict = punify.toAsciiStrict;
+export const toUnicode = punify.toUnicode;
+export const encode = punify.encode;
+export const decode = punify.decode;
+
 export default punify;
